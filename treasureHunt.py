89e9a3b6a115c5b76c1e68fe5b436a178b4b88da
AJ <ajw53@NJIT.edu>
2022-02-01 16:52:12 -0500

    Feature: Added a docstring giving a description for the game.

--- treasureHunt.py
+++ treasureHunt.py
@@ -4,7 +4,15 @@ import random, time
 
 
 def display_game_intro():
-    pass
+    print('''
+            Welcome to the 'Python Treasure Hunt Game'!
+            A game created to help me reinforce the foundations of Python.
+            
+            After a long journey, you find yourself in front of two caves.
+            One cave leads to a treasure, while the other has a spike filled pit.
+            Being you, the player, are a brave treasure hunter looking to pay off a debt,
+            you decide to take the risk!
+        ''')
 
 
 def choose_cave():
@@ -16,4 +24,7 @@ def enter_cave():
 
 
 def main_loop():
-    pass
+    display_game_intro()
+
+
+main_loop()
